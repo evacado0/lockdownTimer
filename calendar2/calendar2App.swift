19523eb3b03db662ce6d5848a7f94bd445351369
evacado14 <68162501+evacado14@users.noreply.github.com>
2023-08-15 14:17:58 -0400

    Initial Commit

---
+++ calendar2/calendar2App.swift
@@ -0,0 +1,17 @@
+//
+//  calendar2App.swift
+//  calendar2
+//
+//  Created by Eva Skarabot on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct calendar2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
